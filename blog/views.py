2ef6513bca83bed9fe0d4164aa396aa7b722c186
ayo43 <ayok43@gmail.com>
2022-04-03 14:18:42 +0100

    moved template to root dir

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    template = 'blog/post_list.html'
+    context = {
+        'posts':posts,
+
+    }
+    return render(request, template_name=template, context=context)
